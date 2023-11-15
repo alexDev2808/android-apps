dc915cad0c50847cff7ef7a126fa7c7b58b87198
alexDev2808 <colores.2808.8000@gmail.com>
2023-11-14 20:59:51 -0600

    Cards para Peso y Edad terminadas

--- app/src/main/java/com/example/androidmaster/IMCCalculator/ImcCalculatorActivity.kt
+++ app/src/main/java/com/example/androidmaster/IMCCalculator/ImcCalculatorActivity.kt
@@ -7,18 +7,27 @@ import android.widget.TextView
 import androidx.cardview.widget.CardView
 import androidx.core.content.ContextCompat
 import com.example.androidmaster.R
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.slider.RangeSlider
 import java.text.DecimalFormat
 
 class ImcCalculatorActivity : AppCompatActivity() {
 
-    private var isMaleSelected : Boolean = true
-    private var isFemaleSelected : Boolean = false
+    private var isMaleSelected: Boolean = true
+    private var isFemaleSelected: Boolean = false
+    private var currentWeight: Int = 40
+    private var currentAge: Int = 12
 
-    private lateinit var viewMale : CardView
-    private lateinit var viewFemale : CardView
+    private lateinit var viewMale: CardView
+    private lateinit var viewFemale: CardView
     private lateinit var tvHeight: TextView
     private lateinit var rsHeight: RangeSlider
+    private lateinit var tvWeight: TextView
+    private lateinit var fabPlusWeight: FloatingActionButton
+    private lateinit var fabSubstractionWeight: FloatingActionButton
+    private lateinit var tvAge: TextView
+    private lateinit var fabPlusAge: FloatingActionButton
+    private lateinit var fabSubstractionAge: FloatingActionButton
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -35,15 +44,24 @@ class ImcCalculatorActivity : AppCompatActivity() {
         viewMale = findViewById(R.id.viewMale)
         tvHeight = findViewById(R.id.tvHeight)
         rsHeight = findViewById(R.id.rsHeight)
+        tvWeight = findViewById(R.id.tvWeight)
+        fabPlusWeight = findViewById(R.id.fabPlusWeight)
+        fabSubstractionWeight = findViewById(R.id.fabSubstractionWeight)
+        tvAge = findViewById(R.id.tvAge)
+        fabPlusAge = findViewById(R.id.fabPlusAge)
+        fabSubstractionAge = findViewById(R.id.fabSubstractionAge)
 
     }
+
     private fun initListeners() {
         viewMale.setOnClickListener {
             changeGender()
-            setGenderColor() }
+            setGenderColor()
+        }
         viewFemale.setOnClickListener {
             changeGender()
-            setGenderColor() }
+            setGenderColor()
+        }
 
         rsHeight.addOnChangeListener { _, value, _ ->
             val df = DecimalFormat("#.##")
@@ -51,6 +69,36 @@ class ImcCalculatorActivity : AppCompatActivity() {
 
             tvHeight.text = "$result cm"
         }
+
+        fabPlusWeight.setOnClickListener {
+            currentWeight += 1
+            setCurrentWeight()
+        }
+
+        fabSubstractionWeight.setOnClickListener {
+            currentWeight -= 1
+            setCurrentWeight()
+
+        }
+
+        fabPlusAge.setOnClickListener {
+            currentAge += 1
+            setCurrentAge()
+        }
+
+        fabSubstractionAge.setOnClickListener {
+            currentAge -= 1
+            setCurrentAge()
+
+        }
+    }
+
+    private fun setCurrentWeight() {
+        tvWeight.text = currentWeight.toString()
+    }
+
+    private fun setCurrentAge() {
+        tvAge.text = currentAge.toString()
     }
 
     private fun changeGender() {
@@ -59,13 +107,14 @@ class ImcCalculatorActivity : AppCompatActivity() {
         isFemaleSelected = !isFemaleSelected
 
     }
+
     private fun setGenderColor() {
         viewMale.setBackgroundColor(getBackgroundColor(isMaleSelected))
         viewFemale.setCardBackgroundColor(getBackgroundColor(isFemaleSelected))
     }
 
-    private fun getBackgroundColor(isSelectedComponent : Boolean): Int {
-        val colorReference = if ( isSelectedComponent ) {
+    private fun getBackgroundColor(isSelectedComponent: Boolean): Int {
+        val colorReference = if (isSelectedComponent) {
             R.color.background_component_selected
         } else {
             R.color.background_component
@@ -76,6 +125,8 @@ class ImcCalculatorActivity : AppCompatActivity() {
 
     private fun initUI() {
         setGenderColor()
+        setCurrentWeight()
+        setCurrentAge()
     }
 
 
